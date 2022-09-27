f8cc926e9f3e6aa2502f2ce5fe960c24914f0d8b
jonny_mo <moirjonathan@gmail.com>
2022-09-27 16:49:25 -0600

    got loading page colors working too!

--- script.js
+++ script.js
@@ -1,21 +1,20 @@
 var htmlElements = "";
-
+//starting 16x16grid for page load
 function startGrid (x){
-for (var i = 0; i < x; i++) {  
-        
+    for (var i = 0; i < x; i++) {  
         htmlElements += '<div class="box"></div>';
         var container = document.getElementById("container");
         container.innerHTML = htmlElements;}
-        
-let boxes = document.querySelectorAll('.box')
-        
-for (let i=0; i<boxes.length; i++) {
-        boxes[i].addEventListener('mouseover', 
-        () => {boxes[i].classList.add('colorIn');});
-        }
-    }
-    
 
+    var boxes = document.getElementsByClassName('box');
+
+    for (var i = 0; i < boxes.length; i++) {
+        boxes[i].onmouseover = function(e) {
+        var backgroundColor = '#'+Math.floor(Math.random()*16777215).toString(16);
+        this.style['background-color'] = backgroundColor;}}
+}
+
+//Custom Grid with user input
 function newGrid (){
     numBoxes = parseInt(prompt ("Pick a number between 1 and 100")); 
     var newHtmlE="";
@@ -23,8 +22,7 @@ function newGrid (){
     for (i=0; i<numBoxes*numBoxes; i++) { 
         newHtmlE += '<div class="box"></div>';
         var container = document.getElementById("container");
-        container.innerHTML = newHtmlE;
-    }
+        container.innerHTML = newHtmlE;}
 
     let gridTemplateColumns = 'repeat('+numBoxes+', 1fr)';
         container.style.gridTemplateColumns = gridTemplateColumns;
