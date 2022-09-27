d921f121bc20c5d0429853f54df0e5a95dbb2eef
jonny_mo <moirjonathan@gmail.com>
2022-09-27 16:45:09 -0600

    got random colors to work!

--- script.js
+++ script.js
@@ -15,47 +15,35 @@ for (let i=0; i<boxes.length; i++) {
         }
     }
     
-    
+
 function newGrid (){
     numBoxes = parseInt(prompt ("Pick a number between 1 and 100")); 
     var newHtmlE="";
+
     for (i=0; i<numBoxes*numBoxes; i++) { 
-        newHtmlE += '<div class="box2"></div>';
-        var container2 = document.getElementById("container2");
-        container2.innerHTML = newHtmlE;
+        newHtmlE += '<div class="box"></div>';
+        var container = document.getElementById("container");
+        container.innerHTML = newHtmlE;
     }
+
     let gridTemplateColumns = 'repeat('+numBoxes+', 1fr)';
-        container2.style.gridTemplateColumns = gridTemplateColumns;
-        container2.style.gridTemplateRows = gridTemplateColumns;
-        
-    let boxes2 = document.querySelectorAll('.box2')
-    for (let i=0; i<boxes2.length; i++) {boxes2[i].addEventListener('mouseover', 
-    () => {boxes2[i].classList.add('colorIn');});
-    }
-    }    
-      
-    
+        container.style.gridTemplateColumns = gridTemplateColumns;
+        container.style.gridTemplateRows = gridTemplateColumns;
+
+    var newBoxes = document.getElementsByClassName('box');
+
+    for (var i = 0; i < newBoxes.length; i++) {
+        newBoxes[i].onmouseover = function(e) {
+        var backgroundColor = '#'+Math.floor(Math.random()*16777215).toString(16);
+        this.style['background-color'] = backgroundColor;}}
+}
 
    
-
-
-
-    /*  
-    totalBoxes = numBoxes * numBoxes;
-    container2.style.gridTemplateColumns = "repeat("+numBoxes+", 2vw)"; 
-    container2.style.gridTemplateRows = "repeat("+numBoxes+", 2vw)";
-
-     
-userPick = prompt ("What size grid would you like?Choose any integer from 1 to 100.");
-
-if (userPick >= 1 && userPick <= 100) {
-var userPick = 16;
-document.getElementById("btn").onclick = function() { 
-        
-    if (userPick >= 1 && userPick <= 100) {
-        for (var i = 0; i < userPick*userPick; i++) {
-            htmlElements += '<div class="box"></div>';
-            var container = document.getElementById("container");
-            container.innerHTML = htmlElements;}}
-    else {
-          return prompt ("That didn't work, pick a different number.");*/
+    
+    
+    
+      
+  
+    
+    
+   
